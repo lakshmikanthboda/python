90970bf4333f90073b919a74805bee2fe7005af7
lakshmikanthboda <60066542+lakshmikanthboda@users.noreply.github.com>
2020-01-24 11:59:30 +0530

    Create arithopponnumpy.py

---
+++ arithopponnumpy.py
@@ -0,0 +1,12 @@
+#########################
+# Arithmatic operations on numpy array
+
+a1=np.array([[1,2,3],[6,6,7]])
+a2=np.array([[9,6,3],[4,8,1.8]])
+print(a1.dtype) # Data type of element sin the numpy array
+print(a2.dtype)
+print(a1+a2)
+print(a1-a2)
+print(a1*a2)
+print(a1/a2)
+print(a1%a2)
