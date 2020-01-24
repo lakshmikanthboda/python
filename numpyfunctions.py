755f92b26c8a6c57b99fdd270c4ffc7c363edcd6
lakshmikanthboda <60066542+lakshmikanthboda@users.noreply.github.com>
2020-01-24 11:26:51 +0530

    Create numpyfunctions.py

---
+++ numpyfunctions.py
@@ -0,0 +1,9 @@
+# NumPy special operatons:
+import numpy as np
+a = np.array([[1,2,3],[2,4,5]])
+print(a.ndim) # returns dimention of the array -- 2
+print(a.shape) # gives shape of numpy
+print(a.size) # gives size
+c= print(a.reshape(3,2)) # to resize the numpy 
+
+###############
